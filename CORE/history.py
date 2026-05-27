5cba9ad4a5c7d353cf69ae8b34d7bcf79fe431ea
Amine Mustapha <mustaphaamine54@gmail.com>
2026-05-27 19:38:38 +0900

    feat: add history export to .log file
    - Implement export_history() with stable speed values from first metric
    - Clean, readable log formatting for all stored metrics
    - Added safety check for empty history
    - Finalized architecture for display, storage, and history modules

--- CORE/history.py
+++ CORE/history.py
@@ -10,6 +10,32 @@ def view_history(collection, n = 5) :
         print(metics)
 
 
+def export_history(collection, filename="history.log"):
+    if not collection:
+        print("No history to export.")
+        return
+
+
+    speed_up = collection[0].up_speed    # use speed of metric
+    speed_down = collection[0].down_speed
+
+    with open(filename, "w") as file:
+        for metric in collection:
+            file.write("\n---------------------------\n")
+            file.write(f"timestamp: {metric.time}\n")
+            file.write(f"cpu: {metric.cpu}\n")
+            file.write(f"ram: {metric.ram}\n")
+            file.write(f"disk total: {metric.disk[0]}\n")
+            file.write(f"disk used: {metric.disk[1]}\n")
+            file.write(f"disk free: {metric.disk[2]}\n")
+            file.write(f"net up: {metric.net_up}\n")
+            file.write(f"net down: {metric.net_down}\n")
+            file.write(f"speed up: {speed_up}\n")
+            file.write(f"speed down: {speed_down}\n")
+            file.write("---------------------------\n")
+
+
+
 
 
 
